5e8ba2f8011da285e58268f80caaa9b22be9680d
Bill.zhanxg <76557923+bill-zhanxg@users.noreply.github.com>
2022-08-21 19:55:50 +1000

    doing file uploading system

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const path = require('path');
 const fs = require('fs');
 const busboy = require('connect-busboy');
 const cheerio = require('cheerio');
-const dir = process.cwd();
 const app = express();
 const config = require('./config.json');
+const iconMapping = require('./iconMapping.json');
 const port = 3001;
 
 process.on('uncaughtException', (err, origin) => {
@@ -113,137 +113,154 @@ function checkPath(id, dir, extra = []) {
 }
 
 app.get('*', async (req, res) => {
-    if (req.session.loggedin) {
-        let userId = `${req.session.data.id}`;
-        let dir = req.params[0];
-        
-        if (!fs.existsSync(checkPath(userId, dir))) return res.sendFile(path.join(__dirname + '/views/not-found.html'));
-        let $ = cheerio.load(await fs.promises.readFile(path.join(__dirname + '/views/user.html')));
-        
-        let folders = [];
-        for (let folder of dir.split('/')) {
-            folders.push(folder);
-            if (folder.trim()) $('.dir').append(`<li><a href="${folders.join('/')}">${folder}</a></li>`);
-        }
+    if (!req.session.loggedin) return res.sendFile(path.join(__dirname + '/views/login.html'));
 
-        if ((await fs.promises.stat(checkPath(userId, dir))).isDirectory()) {
-            fs.readdir(checkPath(userId, dir), async (err, files) => {
-                if (err) {
-                    // Return error element
-                    console.log(err)
+    let userId = `${req.session.data.id}`;
+    let dir = req.params[0];
+
+    if (!fs.existsSync(checkPath(userId, dir))) return res.sendFile(path.join(__dirname + '/views/not-found.html'));
+    let $ = cheerio.load(await fs.promises.readFile(path.join(__dirname + '/views/user.html')));
+
+    let folders = [];
+    for (let folder of dir.split('/')) {
+        if (!folder.trim()) continue;
+        folders.push(folder);
+        if (folder.trim()) $('.dir').append(`<li><a href="/${folders.join('/')}">${folder}</a></li>`);
+    }
+    let currentPath = folders.length > 0 ? `/${folders.join('/')}` : ''
+
+    if ((await fs.promises.stat(checkPath(userId, dir))).isDirectory()) {
+        fs.readdir(checkPath(userId, dir), async (err, files) => {
+            if (err) {
+                // Return error element
+                console.log(err)
+            }
+            else {
+                function appendElement(filename, size, time, icon) {
+                    $('.filePanel').append(`<div class="fileitem cursor-pointer flex justify-between items-center bg-base-200 border-[2px] border-base-300 px-4 py-2 rounded-xl" onclick="intoDir('${currentPath}/${filename}');">
+
+                    <div class="flex basis-1/2 justify-start items-center gap-4">
+                      <input type="checkbox" class="noneClick checkbox checkitem" />
+                      <p class="foldername"><i class="fa-solid fa-${icon} pr-2"></i>${filename}</p>
+                    </div>
+            
+                    <div class="flex basis-1/2 justify-between items-center gap-4 lg:gap-8">
+                      <p>${size}</p>
+                      <p>${time}</p>
+                      <div class="noneClick dropdown dropdown-end">
+                        <label tabindex="0" class="btn btn-info m-1" id="dropdown" onmousedown="closeDropDown(event)"><i class="fa-solid fa-ellipsis-vertical"></i></label>
+                        <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-300 rounded-box w-52" id="dropdown-menu">
+                          <li><a>Do something</a></li>
+                          <li><a>Do something too</a></li>
+                          <li><a class="bg-red-500 text-base-300">just don't press it.</a></li>
+                        </ul>
+                      </div>
+                    </div>
+            
+                  </div>`)
                 }
-                else {
-                    function appendElement(filename, size, time, icon) {
-                        $('.filePanel').append(`<div class="fileitem cursor-pointer flex justify-between items-center bg-base-200 border-[2px] border-base-300 px-4 py-2 rounded-xl">
-
-                        <div class="flex basis-1/2 justify-start items-center gap-4">
-                          <input type="checkbox" class="checkbox checkitem" />
-                          <p class="foldername"><i class="fa-solid fa-${icon} pr-2"></i>${filename}</p>
-                        </div>
-                
-                        <div class="flex basis-1/2 justify-between items-center gap-4 lg:gap-8">
-                          <p>${size}</p>
-                          <p>${time}</p>
-                          <div class="dropdown dropdown-end">
-                            <label tabindex="0" class="btn btn-info m-1" id="dropdown"><i class="fa-solid fa-ellipsis-vertical"></i></label>
-                            <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-300 rounded-box w-52" id="dropdown-menu">
-                              <li><a>Do something</a></li>
-                              <li><a>Do something too</a></li>
-                              <li><a class="bg-red-500 text-base-300">just don't press it.</a></li>
-                            </ul>
-                          </div>
-                        </div>
-                
-                      </div>`)
-                    }
-                    for (let file of files) {
-                        let stat = await fs.promises.stat(checkPath(userId, dir, [file]));
-                        let size = stat.size;
-                        if (size < 1000) size = `${size} Byte${size > 1 ? 's' : ''}`;
-                        else if (size < 1000000) size = `${Math.round(size / 1000)} KB`;
-                        else if (size < 1.0000E+9) size = `${Math.round(size / 1000000)} MB`;
-                        else if (size < 1.0000E+12) size = `${Math.round(size / 1.0000E+9)} GB`;
-                        else size = `${Math.round(size / 1.0000E+12)} TB`;
-                        appendElement(file, size, 'today', stat.isDirectory() ? 'folder': 'file');
-                    }
-                    return res.send($.html());
+                for (let file of files) {
+                    let stat = await fs.promises.stat(checkPath(userId, dir, [file]));
+                    let size = stat.size;
+                    if (size < 1000) size = `${size} Byte${size > 1 ? 's' : ''}`;
+                    else if (size < 1000000) size = `${Math.round(size / 1000)} KB`;
+                    else if (size < 1.0000E+9) size = `${Math.round(size / 1000000)} MB`;
+                    else if (size < 1.0000E+12) size = `${Math.round(size / 1.0000E+9)} GB`;
+                    else size = `${Math.round(size / 1.0000E+12)} TB`;
+                    appendElement(file, size, 'today', stat.isDirectory() ? 'folder' : iconMapping[path.extname(file).slice(1)] || 'file');
                 }
-            })
-        }
-        else {
-
-        }
+                if (files.length < 1) $('.filePanel').append('<h1 class="px-14">There is nothing here! Click "Create Folder" to create a folder!</h1>')
+                $('.createFolder').attr('onclick', `createFolder()`);
+                $('body').attr('currentPath', currentPath);
+                res.send($.html());
+            }
+        })
     }
     else {
-        res.sendFile(path.join(__dirname + '/views/login.html'));
+        // File
+        res.sendFile(checkPath(userId, dir));
     }
+});
+
+app.use('*', (req, res, next) => {
+    if (!req.session.loggedin) return res.status(401).end();
+    res.code = code => res.status(code).end();
+    next();
+});
+
+app.post('/folder/create', (req, res) => {
+    let folderPath = req.body.path;
+    if (!folderPath) return res.code(400);
+    folderPath = checkPath(`${req.session.data.id}`, folderPath);
+    if (fs.existsSync(folderPath)) return res.code(405);
+    fs.mkdir(folderPath, err => {
+        if (err) return res.code(500);
+        res.code(200);
+    });
 });
 
 app.post('/files/upload', (req, res) => {
-    // If the user is loggedin
-    if (req.session.loggedin) {
-        if (!req.session.admin) return;
+    req.pipe(req.busboy); // Pipe it trough busboy
 
-        req.pipe(req.busboy); // Pipe it trough busboy
+    req.busboy.on('field', function (key, value) {
+        req.body[key] = value;
+    });
 
-        req.busboy.on('field', function (key, value) {
-            req.body[key] = value;
-        });
+    // let name = tmp.tmpNameSync();
+    // let stream = fs.createWriteStream(name);
+    req.busboy.on('file', (fieldname, file, info) => {
+        console.log(req.body);
+        // Pipe it trough
+        // file.pipe(stream);
+        // console.log(info);
+    });
 
-        let name = tmp.tmpNameSync();
-        let stream = fs.createWriteStream(name);
-        req.busboy.on('file', (fieldname, file) => {
-            // Pipe it trough
-            file.pipe(stream);
-        });
-
-        req.busboy.on('finish', () => {
-            try {
-                stream.end();
-                let dir = path.join(checkPath(), `${req.body.path}/${req.body.name}`);
-                fs.copyFile(name, dir, () => {
-                    fs.unlink(name, () => { });
-                    res.json({ success: true });
-                });
-            }
-            catch {
-                fs.unlink(name, () => { });
-                res.json({ success: false });
-            }
-        });
-    } else {
-        return res.json({ success: false });
-    }
+    req.busboy.on('finish', () => {
+        // try {
+        //     stream.end();
+        //     let dir = path.join(checkPath(), `${req.body.path}/${req.body.name}`);
+        //     fs.copyFile(name, dir, () => {
+        //         fs.unlink(name, () => { });
+        //         res.json({ success: true });
+        //     });
+        // }
+        // catch {
+        //     fs.unlink(name, () => { });
+        //     res.json({ success: false });
+        // }
+        res.end();
+        console.log(req.body);
+    });
 });
 
-app.post('/files/delete', (req, res) => {
-    // If the user is loggedin
-    if (req.session.loggedin) {
-        if (!req.session.admin) return;
-        let dir = path.join(checkPath(), req.body.path);
-        fs.rm(dir, { recursive: true }, (err) => {
-            if (err) return res.json({ success: false });
-            return res.json({ success: true });
-        });
-    } else {
-        return res.json({ success: false });
-    }
+// Update the file name
+app.put('/files/update', (req, res) => {
+    let oldPath = path.join(checkPath(), req.body.oldPath);
+    let newPath = path.join(checkPath(), req.body.newPath);
+
+    fs.rename(oldPath, newPath, (err) => {
+        if (err) return res.json({ success: false });
+        return res.json({ success: true });
+    })
 });
 
-app.post('/files/rename', (req, res) => {
-    // If the user is loggedin
-    if (req.session.loggedin) {
-        if (!req.session.admin) return;
-        let oldPath = path.join(checkPath(), req.body.oldPath);
-        let newPath = path.join(checkPath(), req.body.newPath);
+// Update the file content
+app.patch('/files/update', (req, res) => {
+    let oldPath = path.join(checkPath(), req.body.oldPath);
+    let newPath = path.join(checkPath(), req.body.newPath);
 
-        fs.rename(oldPath, newPath, (err) => {
-            if (err) return res.json({ success: false });
-            return res.json({ success: true });
-        })
-    } else {
-        return res.json({ success: false });
-    }
+    fs.rename(oldPath, newPath, (err) => {
+        if (err) return res.json({ success: false });
+        return res.json({ success: true });
+    })
+});
+
+app.delete('/files/delete', (req, res) => {
+    let dir = path.join(checkPath(), req.body.path);
+    fs.rm(dir, { recursive: true }, (err) => {
+        if (err) return res.json({ success: false });
+        return res.json({ success: true });
+    });
 });
 
 app.listen(port, () => {
--- static/user.js
+++ static/user.js
@@ -1,20 +1,122 @@
 function logout() {
-    $.post("/auth/logout", () => document.location.reload(true)).fail(() => {
+    $.post("/auth/logout", () => document.location.reload(true)).fail(() => alert('There is an error logging out!'));
+}
 
+$('.noneClick').on('click', event => event.stopPropagation());
+
+function closeDropDown(event) {
+    let targetEl = event.currentTarget;
+    if (targetEl && targetEl.matches(':focus')) {
+        setTimeout(function () {
+            targetEl.blur();
+        }, 0);
+    }
+}
+
+function intoDir(folderName) {
+    window.location = `${folderName}`;
+}
+
+function createFolder() {
+    let currentPath = $('body').attr('currentPath') || '';
+    newFolder(`${currentPath}/test`);
+}
+
+function fileSelector() {
+    let input = document.createElement('input');
+    input.type = 'file';
+    input.multiple = true;
+    input.onchange = e => {
+        uploadFile($('body').attr('currentPath') || '', e.target.files);
+    }
+    input.click();
+}
+
+function newFolder(path) {
+    $.ajax({
+        url: '/folder/create',
+        method: 'POST',
+        data: { path: path },
+        statusCode: {
+            401: () => {
+                alert('Error creating file: you\'re not logged in!');
+                location.reload(true);
+            },
+        },
+        success: () => {
+            location.reload(true);
+        }
     });
 }
 
-$(".fileitem").on('click', event => {
-    let target = $(event.target);
-    if (target.closest('.checkitem').length || target.closest('.dropdown').length) return;
-    let name = target.find('.foldername').text();
-    if (!name.trim()) {
-        if (target.hasClass('foldername')) name = target.text();
-        else return;
-        // MORE CODE REQUIRED HERE
+function uploadFile(path, files) {
+    let data = new FormData();
+    data.append('path', path)
+    for (let file of files) {
+        console.log(file);
+        data.append('file', file);
     }
-    console.log(name);
-    // let name = $(event.target).find('.filename');
-    // console.log(name.text());
-    // window.location = `${window.location.href}/foldername`;
-})
+
+    $.ajax({
+        xhr: function () {
+            var xhr = new XMLHttpRequest();
+            //Upload progress
+            xhr.upload.addEventListener("progress", event => {
+                if (event.lengthComputable) {
+                    var percentComplete = event.loaded / event.total;
+                    //Do something with upload progress
+                    console.log(percentComplete);
+                }
+            }, false);
+            return xhr;
+        },
+        url: '/files/upload',
+        method: 'POST',
+        data: data,
+        contentType: false,
+        processData: false,
+        statusCode: {
+            401: () => {
+                alert('Error uploading file: you\'re not logged in!');
+                location.reload(true);
+            },
+        },
+        success: result => {
+            // location.reload(true);
+        }
+    });
+}
+
+function updateFile(prevLocation, location) {
+    $.ajax({
+        url: '/files/update',
+        method: 'PUT',
+        data: { prevLocation: prevLocation, location: location },
+        statusCode: {
+            401: () => {
+                alert('Error moving/renaming file: you\'re not logged in!');
+                location.reload(true);
+            },
+        },
+        success: result => {
+            location.reload(true);
+        }
+    });
+}
+
+function deleteFile(file) {
+    $.ajax({
+        url: '/files/delete',
+        method: 'DELETE',
+        data: { file: file },
+        statusCode: {
+            401: () => {
+                alert('Error deleting file: you\'re not logged in!');
+                location.reload(true);
+            },
+        },
+        success: result => {
+            location.reload(true);
+        }
+    });
+}
